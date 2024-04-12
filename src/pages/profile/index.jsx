b069d1ab480f5565c5106bc3193fd85cd2875635
Abhimanyu yadav <abhimanyujune3@gamil.com>
2024-04-13 00:33:19 +0530

    fix build

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,5 +1,5 @@
 import { CommingSoon } from 'components/common/index';
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Overview() {
   return <CommingSoon />;

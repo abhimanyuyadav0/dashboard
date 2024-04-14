4d91bb703ee0da9f6e968682a70019d24e697a8c
Abhimanyu yadav <abhimanyujune3@gamil.com>
2024-04-14 19:20:09 +0530

    second release

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ import { store } from 'store';
 import reportWebVitals from './reportWebVitals';
 import { persistor } from 'store/index';
 import { PersistGate } from 'redux-persist/integration/react';
+// import * as firebase from 'firebase/app'; 
+// import * as firebaseStorage from 'firebase/storage';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAqGC6qaHondkbeceKqn36oF1r_faCQ52s",
+//   authDomain: "testing-7e5bf.firebaseapp.com",
+//   databaseURL: "https://testing-7e5bf-default-rtdb.firebaseio.com",
+//   projectId: "testing-7e5bf",
+//   storageBucket: "testing-7e5bf.appspot.com",
+//   messagingSenderId: "458540321787",
+//   appId: "1:458540321787:web:18833890657a4ad73f595e",
+//   measurementId: "G-H28Q81LDZB"
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
@@ -23,4 +39,5 @@ root.render(
     </ReduxProvider>
   </StrictMode>
 );
+
 reportWebVitals();
